a2c6ae1ab4e9b37d3630e79156ead8321c1f9b09
Nguyen Thac Hung <115692325+Zn101@users.noreply.github.com>
2025-08-21 22:34:02 +0700

    Update Exercise06.tsx

--- src/components/Exercise06.tsx
+++ src/components/Exercise06.tsx
@@ -16,13 +16,14 @@ export default function Exercise06() {
   return (    
     <div>
       <label htmlFor="">Gioi tinh</label>
-      <input type="radio" id="Nam" name="gender" onChange={handleChangeInput}/>
+      <input type="radio" id="Nam" name="gender" onChange={handleChangeInput} value="Nam"/>
       <label htmlFor="Nam" >Nam</label>
-      <input type="radio" id="Nu" name="gender" onChange={handleChangeInput}/>
+      <input type="radio" id="Nu" name="gender" onChange={handleChangeInput} value="Nu"/>
       <label htmlFor="Nu">Nu</label>
-      <input type="radio" id="Khac"  name="gender" onChange={handleChangeInput}/>
+      <input type="radio" id="Khac" name="gender" onChange={handleChangeInput} value="Khac"/>
       <label htmlFor="Khac">Khac</label>
       <button onClick={handleSumbit}>Submit</button>
     </div> 
   )
 }
+
